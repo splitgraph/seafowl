6e0e86ac6b1986816333801423e149c1b2e9528a
Artjoms Iskovs <mildbyte@gmail.com>
2022-07-05 19:21:58 +0100

    Inject an ObjectStore into the region struct

--- src/provider.rs
+++ src/provider.rs
@@ -13,6 +13,8 @@ use datafusion::{
     physical_plan::{ExecutionPlan, Partitioning, SendableRecordBatchStream, Statistics},
 };
 
+use object_store::{path::Path, DynObjectStore};
+
 use crate::schema::Schema;
 
 pub struct SeafowlDatabase {
@@ -57,12 +59,15 @@ impl SchemaProvider for SeafowlCollection {
     }
 }
 
+#[derive(Debug)]
 pub struct SeafowlRegion {
     pub object_storage_id: Arc<str>,
     pub row_count: i32,
     pub columns: Arc<Vec<RegionColumn>>,
+    pub object_storage: Arc<DynObjectStore>,
 }
 
+#[derive(Debug)]
 pub struct RegionColumn {
     pub name: Arc<str>,
     pub r#type: Arc<str>,
@@ -143,9 +148,12 @@ impl ExecutionPlan for SeafowlBaseTableScanNode {
 
     fn execute(
         &self,
-        _partition: usize,
+        partition: usize,
         _context: Arc<TaskContext>,
     ) -> Result<SendableRecordBatchStream> {
+        let region = &self.regions[partition];
+        let path = Path::from(region.object_storage_id.as_ref());
+        let _result = region.object_storage.get(&path);
         todo!()
         // Hit the object store up for a certain partition, scan through it
     }
