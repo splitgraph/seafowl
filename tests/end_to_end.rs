c2c814eaa20c2b8d9af557e6e2fb4d6167859925
Artjoms Iskovs <mildbyte@gmail.com>
2022-07-13 14:04:24 +0100

    Add test for `INSERT` (WIP, currently broken)

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -15,7 +15,9 @@ const DEV_DB_DSN: &str = "postgresql://sgr:password@localhost:7432/seafowl";
 /// Make a SeafowlContext that's connected to a real PostgreSQL database
 /// (but uses an in-memory object store)
 async fn make_context_with_pg() -> SeafowlContext {
-    let session_config = SessionConfig::new().with_information_schema(true);
+    let session_config = SessionConfig::new()
+        .with_information_schema(true)
+        .with_default_catalog_and_schema("default", "public");
 
     let context = SessionContext::with_config(session_config);
     let object_store = Arc::new(InMemory::new());
@@ -89,10 +91,12 @@ async fn test_create_table() {
 
     // Check table columns
     let plan = context
-        .plan_query("SELECT table_schema, table_name, column_name, data_type
+        .plan_query(
+            "SELECT table_schema, table_name, column_name, data_type
         FROM information_schema.columns
         WHERE table_catalog = 'default'
-        ORDER BY table_name, column_name")
+        ORDER BY table_name, column_name",
+        )
         .await
         .unwrap();
     let results = context.collect(plan).await.unwrap();
@@ -108,6 +112,56 @@ async fn test_create_table() {
         "| public       | test_table | some_value       | Float32                     |",
         "+--------------+------------+------------------+-----------------------------+",
     ];
+
+    assert_batches_eq!(expected, &results);
+}
+
+#[tokio::test]
+async fn test_create_table_and_insert() {
+    let context = make_context_with_pg().await;
+
+    let plan = context
+        .plan_query(
+            "CREATE TABLE test_table (
+            some_time TIMESTAMP,
+            some_value REAL,
+            some_other_value NUMERIC,
+            some_bool_value BOOLEAN,
+            some_int_value BIGINT)",
+        )
+        .await
+        .unwrap();
+    context.collect(plan).await.unwrap();
+
+    // reregister / reload the catalog
+    context.reload_schema().await;
+
+    // Insert into the table
+    let plan = context
+        .plan_query(
+            "INSERT INTO test_table (some_int_value, some_time, some_value) VALUES
+                (1111, '20:01:01', 42),
+                (2222, '20:02:02', 43),
+                (3333, '20:03:03', 44)",
+        )
+        .await
+        .unwrap();
+    context.collect(plan).await.unwrap();
+
+    // Need to reload the schema to pick up the new table version
+    context.reload_schema().await;
+
+    // Check table columns
+    let plan = context
+        .plan_query("SELECT * FROM test_table")
+        .await
+        .unwrap();
+    // TODO this breaks because of column mismatch here:
+    //   Failed to map column projection for field some_time. Incompatible data types Utf8 and Timestamp(Nanosecond, None)
+    let results = context.collect(plan).await.unwrap();
+
+    let expected =
+        vec!["+--------------+------------+------------------+-----------------------------+"];
 
     assert_batches_eq!(expected, &results);
 }
