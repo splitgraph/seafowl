99f93783bf2aa72bbf8abba3fddfda081520c8d1
Artjoms Iskovs <mildbyte@gmail.com>
2022-07-23 15:08:23 +0100

    Add pre-commit with Prettier and Rust linting

---
+++ ci/clippy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# We have to run Clippy twice here: first, with --fix so that it can fix
+# some issues automatically, then with -D warnings so that it can error out
+# on detected issues. Running it at the same time isn't possible:
+#
+# https://users.rust-lang.org/t/pre-commit-clippy-fix/66584
+
+cargo clippy --all-targets --workspace --fix --allow-dirty --allow-staged
+cargo clippy --all-targets --workspace -- -D warnings
