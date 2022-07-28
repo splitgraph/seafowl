e22b630d641b1dbbda917da1f9a4cdca49d1f0df
Artjoms Iskovs <mildbyte@gmail.com>
2022-07-28 14:31:32 +0100

    Write a macro that implements `Repository`
    See a long comment at the top of repository.rs for an explanation of how we got
    here. Change the PostgresRepository to be in terms of this macro and move the
    migration files to `migrations/postgres`.

---
+++ src/repository/postgres.rs
@@ -0,0 +1,138 @@
+use std::{fmt::Debug, iter::zip, time::Duration};
+
+use async_trait::async_trait;
+use futures::TryStreamExt;
+use sqlx::{
+    migrate::{MigrateDatabase, Migrator},
+    postgres::PgPoolOptions,
+    Error, Executor, PgPool, Postgres, QueryBuilder, Row,
+};
+
+use crate::{
+    data_types::{CollectionId, DatabaseId, PhysicalRegionId, TableId, TableVersionId},
+    implement_repository,
+    provider::{RegionColumn, SeafowlRegion},
+    repository::interface::AllTableRegionsResult,
+    schema::Schema,
+};
+
+use super::{
+    default::RepositoryQueries,
+    interface::{AllDatabaseColumnsResult, Repository},
+};
+
+#[derive(Debug)]
+pub struct PostgresRepository {
+    pub executor: PgPool,
+    pub schema_name: String,
+}
+
+impl PostgresRepository {
+    pub const MIGRATOR: Migrator = sqlx::migrate!("migrations/postgres");
+    pub const QUERIES: RepositoryQueries = RepositoryQueries {
+        all_columns_in_database: r#"
+        WITH latest_table_version AS (
+            SELECT DISTINCT ON (table_id) table_id, id
+            FROM table_version
+            ORDER BY table_id, creation_time DESC, id DESC
+        )
+        SELECT
+            collection.name AS collection_name,
+            "table".name AS table_name,
+            "table".id AS table_id,
+            latest_table_version.id AS table_version_id,
+            table_column.name AS column_name,
+            table_column.type AS column_type
+        FROM collection
+        INNER JOIN "table" ON collection.id = "table".collection_id
+        INNER JOIN latest_table_version ON "table".id = latest_table_version.table_id
+        INNER JOIN table_column ON table_column.table_version_id = latest_table_version.id
+        WHERE collection.database_id = $1
+        ORDER BY collection_name, table_name
+        "#,
+    };
+
+    pub async fn try_new(dsn: String, schema_name: String) -> Result<Self, Error> {
+        if !Postgres::database_exists(&dsn).await? {
+            let _ = Postgres::create_database(&dsn).await;
+        }
+
+        let repo = PostgresRepository::connect(dsn, schema_name.clone()).await?;
+
+        repo.executor
+            .execute(format!("CREATE SCHEMA IF NOT EXISTS {};", schema_name).as_str())
+            .await?;
+
+        // Setup the schema
+        repo.setup().await;
+        Ok(repo)
+    }
+
+    pub async fn connect(dsn: String, schema_name: String) -> Result<Self, Error> {
+        let schema_name_2 = schema_name.clone();
+
+        let pool = PgPoolOptions::new()
+            .min_connections(1)
+            .max_connections(16)
+            .idle_timeout(Duration::from_millis(30000))
+            .test_before_acquire(true)
+            .after_connect(move |c| {
+                let schema_name = schema_name.to_owned();
+                Box::pin(async move {
+                    let query = format!("SET search_path TO {},public;", schema_name);
+                    c.execute(sqlx::query(&query)).await?;
+                    Ok(())
+                })
+            })
+            .connect(&dsn)
+            .await?;
+
+        Ok(Self {
+            executor: pool,
+            schema_name: schema_name_2,
+        })
+    }
+}
+
+implement_repository!(PostgresRepository);
+
+pub mod testutils {
+    use rand::Rng;
+
+    use super::PostgresRepository;
+
+    pub fn get_random_schema() -> String {
+        // Generate a random schema (taken from IOx)
+        let mut rng = rand::thread_rng();
+        (&mut rng)
+            .sample_iter(rand::distributions::Alphanumeric)
+            .filter(|c| c.is_ascii_alphabetic())
+            .take(20)
+            .map(char::from)
+            .collect::<String>()
+    }
+
+    pub async fn make_repository(dsn: &str) -> PostgresRepository {
+        let schema_name = get_random_schema();
+
+        PostgresRepository::try_new(dsn.to_string(), schema_name)
+            .await
+            .expect("Error setting up the database")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, sync::Arc};
+
+    use super::super::interface::tests::run_generic_repository_tests;
+    use super::testutils::make_repository;
+
+    #[tokio::test]
+    async fn test_postgres_repository() {
+        let dsn = env::var("DATABASE_URL").unwrap();
+        let repository = Arc::new(make_repository(&dsn).await);
+
+        run_generic_repository_tests(repository).await;
+    }
+}
