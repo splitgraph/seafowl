71a45afbe1e3caba39ecd810a0f7e0c6fcaf68a6
Artjoms Iskovs <mildbyte@gmail.com>
2022-08-23 09:09:36 +0100

    Add envvar config parsing
    Use e.g. `SEAFOWL__FRONTEND__HTTP__WRITE_ACCESS` as an envvar to
    partially override the config.

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -33,7 +33,7 @@ schema = "{}""#,
 
     // Ignore the "in-memory object store / persistent catalog" error in e2e tests (we'll discard
     // the PG instance anyway)
-    let config = load_config_from_string(&config_text, true).unwrap();
+    let config = load_config_from_string(&config_text, true, None).unwrap();
     build_context(&config).await.unwrap()
 }
 
