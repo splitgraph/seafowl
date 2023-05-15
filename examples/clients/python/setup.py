2e074f41c09f29c1b2e59d0ccaa51c88b6c3f533
Peter Neumark <neumark.peter@gmail.com>
2023-05-15 10:55:36 +0200

    Add python Seafowl client library

---
+++ examples/clients/python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name="python-seafowl-client",
+      version="0.0.1",
+      description="Python client for the Seafowl analytical SQL database.",
+      license='Apache 2.0',
+      author="Splitgraph, Inc",
+      authoer_email="hello@splitgraph.com",
+      url='https://github.com/matiascodesal/git-for-pip-example',
+      packages=['seafowl']
+)
